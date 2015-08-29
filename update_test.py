ad4c8cf674051e37b7e3909ee186f50e4017a0b9
kalininei <kalininei@yandex.ru>
2015-08-29 22:04:17 +0300

    3th commit

--- update_test.py
+++ update_test.py
@@ -2,4 +2,15 @@
 version = "0.2"
 
 print "Version: %s" % version
-print "Done"
+
+rurl = 'https://api.github.com/repos/kalininei/TestingSandBox/releases/latest'
+
+import urllib2
+import json
+
+try:
+    response = urllib2.urlopen(rurl, timeout=1)
+    r = json.loads(response.read())
+    print r['tag_name'], r['html_url']
+except:
+    print (None, None)
